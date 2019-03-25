d105d5362e154d8a8bc587cd375911043150ad90
nikolaenkoOleg <mnks@list.ru>
2019-03-25 15:56:10 +1000

    добавлен плагин для прокрутки и немного адаптива

--- price.php
+++ price.php
@@ -21,9 +21,11 @@
 	<!-- Custom Browsers Color End -->
 
 	<link rel="stylesheet" href="../css/normalize.css">
+	<script type="text/javascript" src="libs/jquery/dist/jquery-1.9.1.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/main.min.css">
 	<link rel="stylesheet" href="css/hamburger.css">
+<!-- 	<link rel="stylesheet" href="libs/revealator/fm.revealator.jquery.min.css"> -->
 </head>
 <body>
 	<aside>
@@ -32,92 +34,101 @@
 		<?php require('template/footer.php') ?>
 	</aside>
 	<div class="price">
-		<h1 class="price__head">Прайс</h1>
+		<div class="price__wrapper">
+			<h1 class="price__head">Прайс</h1>
 
-		<div class="price__block">
-			<div class="price__img">
-				<figure>
-					<img src="upload/thumbnails/018.jpg" alt="price-img">
-				</figure>
+			<div class="price__block revealator-slideup revealator-once">
+				<div class="price__img-block">
+					<figure>
+						<img src="upload/thumbnails/018.jpg" alt="price-img" class="price__img">
+					</figure>
+				</div>
+				<div class="price__description">
+					<h2 class="price__name">Индивидуальная</h2>
+					<p class="price__info">1 человек 2000 руб/час-полтора</p>
+					<p class="price__info">Love-story, фотосессия подруг, друзей (до 3х человек) -2500 руб/час (каждый последующий человек +200)</p>
+					<ul class="price__list">
+						<li class="price__list-elem">10 фото в полной обработке</li>
+						<li class="price__list-elem">Все оставшиеся в цветокоррекии</li>
+						<li class="price__list-elem">Готовность в течение 10 рабочих дней</li>
+						<li class="price__list-elem">Отправляю облаком</li>
+					</ul>
+				</div>
 			</div>
-			<div class="price__description">
-				<h2 class="price__name">Индивидуальная</h2>
-				<p class="price__info">1 человек 2000 руб/час-полтора</p>
-				<p class="price__info">Love-story, фотосессия подруг, друзей (до 3х человек) -2500 руб/час (каждый последующий человек +200)</p>
-				<ul class="price__list">
-					<li class="price__list-elem">10 фото в полной обработке;</li>
-					<li class="price__list-elem">Все оставшиеся в цветокоррекии;</li>
-					<li class="price__list-elem">Готовность в течение 10 рабочих дней;</li>
-					<li class="price__list-elem">Отправляю облаком.</li>
-				</ul>
-			</div>
-		</div>
 
-		<div class="price__block">
-			<div class="price__img">
-				<figure>
-					<img src="upload/thumbnails/019.jpg" alt="price-img">
-				</figure>				
+			<div class="price__block revealator-slideup revealator-once">
+				<div class="price__img">
+					<figure>
+						<img src="upload/thumbnails/019.jpg" alt="price-img" class="price__img">
+					</figure>				
+				</div>
+				<div class="price__description">
+					<h2 class="price__name">Репортажная съемка</h2>
+					<p class="price__info">Мероприятия, банкеты 2000 руб/час</p>
+					<ul class="price__list">
+						<li class="price__list-elem">Все фото с цветокоррекцией и ретушью крупных планов</li>
+						<li class="price__list-elem">Готовность в течение 10 рабочих дней</li>
+						<li class="price__list-elem">Отправляю облаком</li>
+					</ul>
+				</div>
 			</div>
-			<div class="price__description">
-				<h2 class="price__name">Репортажная съемка</h2>
-				<p class="price__info">Мероприятия, банкеты 2000 руб/час</p>
-				<ul class="price__list">
-					<li class="price__list-elem">Все фото с цветокоррекцией и ретушью крупных планов;</li>
-					<li class="price__list-elem">Готовность в течение 10 рабочих дней;</li>
-					<li class="price__list-elem">Jтправляю облаком.</li>
-				</ul>
-			</div>
-		</div>
 
-		<div class="price__block">
-			<div class="price__img">
-				<figure>
-					<img src="upload/thumbnails/020.jpg" alt="price-img">
-				</figure>	
+			<div class="price__block revealator-slideup revealator-once">
+				<div class="price__img">
+					<figure>
+						<img src="upload/thumbnails/020.jpg" alt="price-img" class="price__img">
+					</figure>	
+				</div>
+				<div class="price__description">
+					<h2 class="price__name">Свадьба</h2>
+					<p class="price__info">Загс, прогулка молодожёнов 1000 руб/час</p>
+					<p class="price__info">Сьёмка банкета 1300 руб/час </p>
+					<p class="price__info">Минимальное количество часов свадебной фотосессии - 3!</p>
+					<ul class="price__list">
+						<li class="price__list-elem">Все фото с цветокоррекцией и ретушью крупных планов</li>
+						<li class="price__list-elem">Готовность в течение 30 календарных дней</li>
+						<li class="price__list-elem">Отправляю облаком</li>
+					</ul>
+				</div>
 			</div>
-			<div class="price__description">
-				<h2 class="price__name">Свадьба</h2>
-				<p class="price__info">Загс, прогулка молодожёнов 1000 руб/час</p>
-				<p class="price__info">Сьёмка банкета 1300 руб/час </p>
-				<p class="price__info">Минимальное количество часов свадебной фотосессии - 3!</p>
-				<ul class="price__list">
-					<li class="price__list-elem">Все фото с цветокоррекцией и ретушью крупных планов;</li>
-					<li class="price__list-elem">Готовность в течение 30 календарных дней;</li>
-					<li class="price__list-elem">Отправляю облаком.</li>
-				</ul>
-			</div>
-		</div>
 
-		<div class="price__block">
-			<div class="price__img">
-				<figure>
-					<img src="upload/thumbnails/021.jpg" alt="price-img">
-				</figure>			
+			<div class="price__block revealator-slideup revealator-once">
+				<div class="price__img">
+					<figure>
+						<img src="upload/thumbnails/021.jpg" alt="price-img" class="price__img">
+					</figure>			
+				</div>
+				<div class="price__description">
+					<h2 class="price__name">Предметная, фуд-фотосессия, сьёмка для магазинов одежды</h2>
+					<p class="price__info">Цены варьируются в зависимости от конкретной задачи, пишите, уточняйте.</p>
+				</div>
 			</div>
-			<div class="price__description">
-				<h2 class="price__name">Предметная, фуд-фотосессия, сьёмка для магазинов одежды</h2>
-				<p class="price__info">Цены варьируются в зависимости от конкретной задачи, пишите, уточняйте.</p>
+
+			<hr>
+
+			<div class="certificate">
+				<div class="certificate__wrapper">
+					<h2 class="certificate__name">Подарочный сертификат</h2>
+					<p class="certificate__info">Возможно приобрести подарочный сертификат на фотосессию. Это отличный подарок на любой праздник. Получатель сможет воспользоваться им в любое удобное для него время, предварительно обсудив детали с фотографом. Без сомнения, такой подарок оставит только счастливые воспоминания и теплоту ♥</p>
+					<p class="certificate__info">Срок действия сертификата 3 месяца с момента приобретения.</p>
+				</div>
 			</div>
-		</div>
 
-		<div class="price__certificate">
-			<h2 class="price__certificate-name">Подарочный сертификат</h2>
-			<p class="price__certificate-info">Возможно приобрести подарочный сертификат на фотосессию. Это отличный подарок на любой праздник. Получатель сможет воспользоваться им в любое удобное для него время, предварительно обсудив детали с фотографом. Без сомнения, такой подарок оставит только счастливые воспоминания и теплоту ♥</p>
-			<p class="price__certificate-info">Срок действия сертификата 3 месяца с момента приобретения.</p>
-		</div>
-
-		<div class="price__extra-services">
-			<h2 class="price__extra-services-name">Дополнительные услуги:</h2>
-			<ul class="price__extra-services-list">
-				<li class="price__extra-services-list-elem">Визажист от 1000 руб;</li>
-				<li class="extra-services__list-elem">Прокат платьев от 1000 руб;</li>
-				<li class="extra-services__list-elem">Фото студия от 800-2000 руб/час;</li>
-				<li class="extra-services__list-elem">Ретушь дополнительного кадра 200 руб;</li>
-				<li class="extra-services__list-elem">Животные (доберманы, Маламуты) от руб/час;</li>
-				<li class="extra-services__list-elem">Срочная обработка фотографий в течение двух дней +1 000 руб.</li>
-			</ul>
+			<div class="extra-services">
+				<div class="extra-services__wrapper">
+					<h2 class="extra-services__name">Дополнительные услуги:</h2>
+					<ul class="extra-services__list">
+						<li class="extra-services__list-elem">Визажист от 1000 руб</li>
+						<li class="extra-services__list-elem">Прокат платьев от 1000 руб</li>
+						<li class="extra-services__list-elem">Фото студия от 800-2000 руб/час</li>
+						<li class="extra-services__list-elem">Ретушь дополнительного кадра 200 руб</li>
+						<li class="extra-services__list-elem">Животные (доберманы, Маламуты) от руб/час</li>
+						<li class="extra-services__list-elem">Срочная обработка фотографий в течение двух дней +1 000 руб</li>
+					</ul>
+				</div>
+			</div>			
 		</div>
 	</div>
+<!-- 	<script src="libs/revealator/fm.revealator.jquery.min.js"></script> -->
 </body>
 </html>
